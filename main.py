e07938309a594d1ad988f6d1823bf860a31ccc99
Tommy Lin <tommylinkl6@gmail.com>
2024-02-12 22:39:47 -0800

    Cleaned up main.py using code blocks. Cleaned up ham class functions

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from utils.read import MEMORY_FLAG, RUNTIME_FLAG, BulkSystem, read_NNConfigFile,
 from utils.pp_func import plotPP, FT_converge_and_write_pp, plotBandStruct
 from utils.init_NN_train import init_Zunger_data, init_Zunger_weighted_mse, init_Zunger_train_GPU
 from utils.NN_train import weighted_mse_bandStruct, weighted_mse_energiesAtKpt, bandStruct_train_GPU
-from utils.ham import Hamiltonian
+from utils.ham import Hamiltonian, initAndCacheHams
 
 def main(inputsFolder = 'inputs/', resultsFolder = 'results/'):
     torch.set_num_threads(1)
@@ -36,88 +36,20 @@ def main(inputsFolder = 'inputs/', resultsFolder = 'results/'):
     # Set up the neural network
     PPmodel = setNN(NNConfig, nPseudopot)
 
-    # Initialize the ham class for each BulkSystem. 
-    # dummy_ham is used to initialize and store the cached SOmats and NLmats in dict cachedMats. 
-    # As I initialize dummy_ham, immediately load them into share memory
-    # Use a dict "cachedMats_info" to store dtype and shape
-    # Then remove dummy_ham, and any intermediate variables
-    print("\nInitializing the ham class for each BulkSystem. ")
-    hams = []
-    cachedMats_info = None
-    shm_dict_SO = None
-    shm_dict_NL = None
-    for iSys in range(nSystem): 
-        start_time = time.time()
-
-        # Here I separate: 
-        # 1. SObool = False --> Just initialize ham. No storage / moving is needed. 
-        # 2. SObool = True, no parallel --> Initialize ham with cache. No storage / moving is needed.
-        # 3. SObool = True, yes parallel --> Do the complicated storage / moving. 
-        if not NNConfig['SObool']: 
-            ham = Hamiltonian(systems[iSys], PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
-        elif (NNConfig['SObool']) and ('num_cores' not in NNConfig): 
-            ham = Hamiltonian(systems[iSys], PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
-        else: 
-            cachedMats_info = {}
-            shm_dict_SO = {}
-            shm_dict_NL = {}
-            ham = Hamiltonian(systems[iSys], PPparams, atomPPOrder, device, NNConfig, SObool=True, cacheSO=False)
-            dummy_ham = Hamiltonian(systems[iSys], PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
-
-            if dummy_ham.SOmats is not None: 
-                # reshape dummy_ham.SOmats into 4D arrays 
-                # of shape (nkpt)*(nAtoms)*(2*nbasis) x (2*nbasis)
-                tmpSOmats = dummy_ham.SOmats
-                tmpSOmats_4d = np.array(tmpSOmats.tolist(), dtype=np.complex128).reshape((tmpSOmats.shape[0], tmpSOmats.shape[1], tmpSOmats[0,0].shape[0], tmpSOmats[0,0].shape[1]))
-                for kidx in range(systems[iSys].getNKpts()):
-                    SOkey = f"SO_{iSys}_{kidx}"
-                    SOvalue = {'dtype': tmpSOmats_4d[kidx].dtype,
-                        'shape': tmpSOmats_4d[kidx].shape,
-                    }
-                    cachedMats_info[SOkey] = SOvalue
-
-                    # Move the SOmats to shared memory
-                    shm_dict_SO[f"shm_SO_{iSys}_{kidx}"] = shared_memory.SharedMemory(create=True, size=tmpSOmats_4d[kidx].nbytes, name=f"SOmats_{iSys}_{kidx}")
-                    tmp_arr = np.ndarray(cachedMats_info[f"SO_{iSys}_{kidx}"]['shape'], dtype=cachedMats_info[f"SO_{iSys}_{kidx}"]['dtype'], buffer=shm_dict_SO[f"shm_SO_{iSys}_{kidx}"].buf)  # Create a NumPy array backed by shared memory
-                    tmp_arr[:] = tmpSOmats_4d[kidx][:]   # Copy the cached SOmat into shared memory
-
-                del tmpSOmats, tmpSOmats_4d
-            if dummy_ham.NLmats is not None: 
-                # reshape dummy_ham.NLmats into 5D arrays 
-                # of shape (nkpt)*(nAtoms)*(2)*(2*nbasis) x (2*nbasis)
-                tmpNLmats = dummy_ham.NLmats
-                tmpNLmats_5d = np.array(tmpNLmats.tolist(), dtype=np.complex128).reshape((tmpNLmats.shape[0], tmpNLmats.shape[1], tmpNLmats.shape[2], tmpNLmats[0,0,0].shape[0], tmpNLmats[0,0,0].shape[1]))
-                for kidx in range(systems[iSys].getNKpts()):
-                    NLkey = f"NL_{iSys}_{kidx}"
-                    NLvalue = {'dtype': tmpNLmats_5d[kidx].dtype,
-                        'shape': tmpNLmats_5d[kidx].shape,
-                    }
-                    cachedMats_info[NLkey] = NLvalue
-
-                    # Move the NLmats to shared memory
-                    shm_dict_NL[f"shm_NL_{iSys}_{kidx}"] = shared_memory.SharedMemory(create=True, size=tmpNLmats_5d[kidx].nbytes, name=f"NLmats_{iSys}_{kidx}")
-                    tmp_arr = np.ndarray(cachedMats_info[f"NL_{iSys}_{kidx}"]['shape'], dtype=cachedMats_info[f"NL_{iSys}_{kidx}"]['dtype'], buffer=shm_dict_NL[f"shm_NL_{iSys}_{kidx}"].buf) 
-                    tmp_arr[:] = tmpNLmats_5d[kidx][:] 
-
-                del tmpNLmats, tmpNLmats_5d
-            del dummy_ham
-            gc.collect()
-            print("Finished putting the cached SO and NLmats into shared memory ...")
-        hams.append(ham)
-        end_time = time.time()
-        print(f"Elapsed time: {(end_time - start_time):.2f} seconds\n")
+    # Initialize the ham class for each BulkSystem. Cache the SO and NL mats. 
+    hams, cachedMats_info, shm_dict_SO, shm_dict_NL = initAndCacheHams(systems, NNConfig, PPparams, atomPPOrder, device)
 
     oldFunc_plot_bandStruct_list = []
     oldFunc_totalMSE = 0
-    for iSystem in range(nSystem): 
+    for iSys, sys in enumerate(systems):
         start_time = time.time()
-        oldFunc_bandStruct = hams[iSystem].calcBandStruct_noGrad(NNConfig, iSystem, cachedMats_info if cachedMats_info is not None else None)
+        oldFunc_bandStruct = hams[iSys].calcBandStruct_noGrad(NNConfig, iSys, cachedMats_info if cachedMats_info is not None else None)
         oldFunc_bandStruct.detach_()
         end_time = time.time()
-        print(f"Old Zunger BS: Finished calculating {iSystem}-th band structure in the Zunger function form ... Elapsed time: {(end_time - start_time):.2f} seconds")
-        oldFunc_plot_bandStruct_list.append(systems[iSystem].expBandStruct)
+        print(f"Old Zunger BS: Finished calculating {iSys}-th band structure in the Zunger function form ... Elapsed time: {(end_time - start_time):.2f} seconds")
+        oldFunc_plot_bandStruct_list.append(sys.expBandStruct)
         oldFunc_plot_bandStruct_list.append(oldFunc_bandStruct)
-        oldFunc_totalMSE += weighted_mse_bandStruct(oldFunc_bandStruct, systems[iSystem])
+        oldFunc_totalMSE += weighted_mse_bandStruct(oldFunc_bandStruct, sys)
     fig = plotBandStruct(systems, oldFunc_plot_bandStruct_list, NNConfig['SHOWPLOTS'])
     print("The total bandStruct MSE = %e " % oldFunc_totalMSE)
     fig.suptitle("The total bandStruct MSE = %e " % oldFunc_totalMSE)
--- utils/ham.py
+++ utils/ham.py
@@ -7,7 +7,7 @@ import time
 from torch.utils.checkpoint import checkpoint
 import multiprocessing as mp
 from multiprocessing import Process, Queue, Pool, shared_memory
-from functools import partial
+import gc
 
 from constants.constants import *
 from utils.pp_func import pot_func
@@ -798,9 +798,9 @@ class Hamiltonian:
 
 
     def calcBandStruct_noGrad(self, NNConfig, iSystem, cachedMats_info):
-        '''
+        """
         Multiprocessing is implemented. However, the returned bandStruct doesn't have gradients.
-        '''
+        """
         
         nbands = self.system.nBands
         nkpt = self.system.getNKpts()
@@ -815,7 +815,7 @@ class Hamiltonian:
             torch.set_num_threads(1)
             os.environ["OMP_NUM_THREADS"] = "1"
             os.environ["MKL_NUM_THREADS"] = "1"
-            print(f"The size of cachedMats_info is: {sys.getsizeof(cachedMats_info)/1024} KB")
+            # print(f"The size of cachedMats_info is: {sys.getsizeof(cachedMats_info)/1024} KB")
             args_list = [(kidx, iSystem, cachedMats_info, False) for kidx in range(nkpt)]
             with mp.Pool(NNConfig['num_cores']) as pool:
                 eigValsList = pool.starmap(self.calcEigValsAtK, args_list)
@@ -1003,7 +1003,7 @@ class Hamiltonian:
     def get_PPparams(self):
         return self.PPparams
     
-    
+
     def set_PPparams(self, newparams):
         """
         Set new values for the algebraic PP "a" params.
@@ -1012,3 +1012,78 @@ class Hamiltonian:
         """
         self.PPparams = newparams
 
+
+def initAndCacheHams(systemsList, NNConfig, PPparams, atomPPOrder, device):
+    """
+    Initialize the ham class for each BulkSystem. 
+    dummy_ham is used to initialize and store the cached SOmats and NLmats in dict cachedMats. 
+    As I initialize dummy_ham, immediately load them into share memory
+    Use a dict "cachedMats_info" to store dtype and shape
+    Then remove dummy_ham, and any intermediate variables
+    """
+    print("\nInitializing the ham class for each BulkSystem. Cache-ing the corresponding SO and NL mats. ")
+    hams = []
+    cachedMats_info = None
+    shm_dict_SO = None
+    shm_dict_NL = None
+    for iSys, sys in enumerate(systemsList):
+        start_time = time.time()
+
+        # Here I separate: 
+        # 1. SObool = False --> Just initialize ham. No storage / moving is needed. 
+        # 2. SObool = True, no parallel --> Initialize ham with cache. No storage / moving is needed.
+        # 3. SObool = True, yes parallel --> Do the complicated storage / moving. 
+        if not NNConfig['SObool']: 
+            ham = Hamiltonian(sys, PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
+        elif (NNConfig['SObool']) and (('num_cores' not in NNConfig) or (NNConfig['num_cores']==0)): 
+            ham = Hamiltonian(sys, PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
+        else: 
+            cachedMats_info = {}
+            shm_dict_SO = {}
+            shm_dict_NL = {}
+            ham = Hamiltonian(sys, PPparams, atomPPOrder, device, NNConfig, SObool=True, cacheSO=False)
+            dummy_ham = Hamiltonian(sys, PPparams, atomPPOrder, device, NNConfig, SObool=NNConfig['SObool'])
+
+            if dummy_ham.SOmats is not None: 
+                # reshape dummy_ham.SOmats into 4D arrays 
+                # of shape (nkpt)*(nAtoms)*(2*nbasis) x (2*nbasis)
+                tmpSOmats = dummy_ham.SOmats
+                tmpSOmats_4d = np.array(tmpSOmats.tolist(), dtype=np.complex128).reshape((tmpSOmats.shape[0], tmpSOmats.shape[1], tmpSOmats[0,0].shape[0], tmpSOmats[0,0].shape[1]))
+                for kidx in range(sys.getNKpts()):
+                    SOkey = f"SO_{iSys}_{kidx}"
+                    SOvalue = {'dtype': tmpSOmats_4d[kidx].dtype,
+                        'shape': tmpSOmats_4d[kidx].shape,
+                    }
+                    cachedMats_info[SOkey] = SOvalue
+
+                    # Move the SOmats to shared memory
+                    shm_dict_SO[f"shm_SO_{iSys}_{kidx}"] = shared_memory.SharedMemory(create=True, size=tmpSOmats_4d[kidx].nbytes, name=f"SOmats_{iSys}_{kidx}")
+                    tmp_arr = np.ndarray(cachedMats_info[f"SO_{iSys}_{kidx}"]['shape'], dtype=cachedMats_info[f"SO_{iSys}_{kidx}"]['dtype'], buffer=shm_dict_SO[f"shm_SO_{iSys}_{kidx}"].buf)  # Create a NumPy array backed by shared memory
+                    tmp_arr[:] = tmpSOmats_4d[kidx][:]   # Copy the cached SOmat into shared memory
+
+                del tmpSOmats, tmpSOmats_4d
+            if dummy_ham.NLmats is not None: 
+                # reshape dummy_ham.NLmats into 5D arrays 
+                # of shape (nkpt)*(nAtoms)*(2)*(2*nbasis) x (2*nbasis)
+                tmpNLmats = dummy_ham.NLmats
+                tmpNLmats_5d = np.array(tmpNLmats.tolist(), dtype=np.complex128).reshape((tmpNLmats.shape[0], tmpNLmats.shape[1], tmpNLmats.shape[2], tmpNLmats[0,0,0].shape[0], tmpNLmats[0,0,0].shape[1]))
+                for kidx in range(sys.getNKpts()):
+                    NLkey = f"NL_{iSys}_{kidx}"
+                    NLvalue = {'dtype': tmpNLmats_5d[kidx].dtype,
+                        'shape': tmpNLmats_5d[kidx].shape,
+                    }
+                    cachedMats_info[NLkey] = NLvalue
+
+                    # Move the NLmats to shared memory
+                    shm_dict_NL[f"shm_NL_{iSys}_{kidx}"] = shared_memory.SharedMemory(create=True, size=tmpNLmats_5d[kidx].nbytes, name=f"NLmats_{iSys}_{kidx}")
+                    tmp_arr = np.ndarray(cachedMats_info[f"NL_{iSys}_{kidx}"]['shape'], dtype=cachedMats_info[f"NL_{iSys}_{kidx}"]['dtype'], buffer=shm_dict_NL[f"shm_NL_{iSys}_{kidx}"].buf) 
+                    tmp_arr[:] = tmpNLmats_5d[kidx][:] 
+
+                del tmpNLmats, tmpNLmats_5d
+            del dummy_ham
+            gc.collect()
+            print("Finished putting the cached SO and NLmats into shared memory ...")
+        hams.append(ham)
+        end_time = time.time()
+        print(f"Elapsed time: {(end_time - start_time):.2f} seconds\n")
+    return hams, cachedMats_info, shm_dict_SO, shm_dict_NL
