637ca2061971f977d23e1e02099ce7e35a4a5beb
Tommy Lin <tommylinkl6@gmail.com>
2024-07-02 22:00:55 -0700

    Eigenvector overlap for 32, 64, 128, 150 kpts.

--- test_overlap.py
+++ test_overlap.py
@@ -30,20 +30,7 @@ def overlap_2eigVec(a, b):
     return np.abs(overlap)
 
 
-def initOverlapMatrix(nbands, deg=1): 
-    if deg==1: 
-        M = np.zeros((nbands, nbands), dtype=np.float64)
-    elif deg==2: 
-        if nbands % 2 != 0: 
-            raise ValueError("There are odd number of bands, for the degeneracy = 2 case! Stop!")
-        M = np.zeros((int(nbands/2), int(nbands/2)), dtype=np.float64)
-    else: 
-        raise ValueError("Error! We currently only support calculating the overlap matrix for singly- or doubly-degenerate states!")
-    
-    return M
-
-
-def overlapMatrix(kIdx1, kIdx2, deg=1, verbosity=0): 
+def overlapMatrix(kIdx1, kIdx2): 
     evec_list_kIdx1 = read_npz(f"CALCS/CsPbI3_test/results_{testCase}kpts/eigVec_k{kIdx1}.npz")
     evec_list_kIdx2 = read_npz(f"CALCS/CsPbI3_test/results_{testCase}kpts/eigVec_k{kIdx2}.npz")
     if len(evec_list_kIdx1) != len(evec_list_kIdx2):
@@ -51,30 +38,65 @@ def overlapMatrix(kIdx1, kIdx2, deg=1, verbosity=0):
     else: 
         nbands = len(evec_list_kIdx1)
 
-    M = initOverlapMatrix(nbands, deg)
-    if deg==1: 
-        for i in range(nbands):
-            for j in range(nbands): 
-                M[i,j] = overlap_2eigVec(evec_list_kIdx1[i], evec_list_kIdx2[j])
-    elif deg==2: 
-        for i in range(int(nbands/2)):
-            for j in range(int(nbands/2)): 
-                # M[i,j] = max(overlap_2eigVec(evec_list_kIdx1[2*i], evec_list_kIdx2[2*j]) + overlap_2eigVec(evec_list_kIdx1[2*i+1], evec_list_kIdx2[2*j+1]), overlap_2eigVec(evec_list_kIdx1[2*i], evec_list_kIdx2[2*j+1]) + overlap_2eigVec(evec_list_kIdx1[2*i+1], evec_list_kIdx2[2*j]))
-                M[i,j] = overlap_2eigVec(evec_list_kIdx1[2*i], evec_list_kIdx2[2*j]) + overlap_2eigVec(evec_list_kIdx1[2*i+1], evec_list_kIdx2[2*j+1]) + overlap_2eigVec(evec_list_kIdx1[2*i], evec_list_kIdx2[2*j+1]) + overlap_2eigVec(evec_list_kIdx1[2*i+1], evec_list_kIdx2[2*j])
-
-    if verbosity>0: 
-        diff = M - np.eye(M.shape[0])
-        if np.allclose(diff, 0, atol=1e-4):
-            print("YES! The overlap matrix is effectively identity.")
-        else:
-            print("NO! The overlap matrix is not close to identity.")
-
-    # np.set_printoptions(formatter={'float': lambda x: "{: .3f}".format(x)})
-    # print(M)
-    if deg==1:
-        np.savetxt(f"CALCS/CsPbI3_test/results_{testCase}kpts/overlap_kIdx_{kIdx1}_{kIdx2}_CBQuad.dat", M[-6:,-6:], fmt='%.4f')
+    M = np.zeros((nbands, nbands), dtype=np.float64)
+    for i in range(nbands):
+        for j in range(nbands): 
+            M[i,j] = overlap_2eigVec(evec_list_kIdx1[i], evec_list_kIdx2[j])
+    
+    np.savetxt(f"CALCS/CsPbI3_test/results_{testCase}kpts/reorder_results/overlap_kIdx_{kIdx1}_{kIdx2}_CBQuad.dat", M[-6:,-6:], fmt='%.4f')
+    plotOverlapMatrix(M, f"CALCS/CsPbI3_test/results_{testCase}kpts/reorder_results/plotOverlap_kIdx_{kIdx1}_{kIdx2}.png")
 
     return M
+
+
+def plotOverlapMatrix(M, plotFilename):
+    plt.figure(figsize=(10, 10)) 
+    plt.imshow(M, cmap='Reds', vmin=0, vmax=1)
+    plt.colorbar()
+
+    # Adding lighter dashed grid lines
+    for x in np.arange(-0.5, 32, 1):
+        plt.axhline(x, color='lightgrey', linestyle='--', linewidth=0.5)
+        plt.axvline(x, color='lightgrey', linestyle='--', linewidth=0.5)
+
+    # Overlaying solid lines for every second grid line
+    for x in np.arange(0, 32, 2):
+        plt.axhline(x - 0.5, color='black', linestyle='-', linewidth=2)
+        plt.axvline(x - 0.5, color='black', linestyle='-', linewidth=2)
+
+    # Highlighting the CB's, i.e., bottom right 6x6 quadrant
+    highlight_start = 25.5
+    highlight_end = 31.5
+    plt.axhline(highlight_start, color='blue', linestyle='-', linewidth=3)
+    plt.axhline(highlight_end, color='blue', linestyle='-', linewidth=3)
+    plt.axvline(highlight_start, color='blue', linestyle='-', linewidth=3)
+    plt.axvline(highlight_end, color='blue', linestyle='-', linewidth=3)
+
+    # Setting ticks for grid lines and labeling every 4 grids
+    tick_positions = np.arange(0, 33, 4)
+    plt.xticks(tick_positions - 0.5, labels=np.arange(0, 33, 4))
+    plt.yticks(tick_positions - 0.5, labels=np.arange(0, 33, 4))
+
+    plt.savefig(plotFilename, bbox_inches='tight')
+    plt.close()
+
+
+def contractM_deg2(M, mode='sum'): 
+    if M.shape[0] % 2 != 0: 
+        raise ValueError("Requesting to contract the overlap matrix by deg=2, but there are odd number of bands! Stop!")
+    else:
+        deg2_M = np.zeros((int(M.shape[0]/2), int(M.shape[0]/2)), dtype=np.float64)
+    
+    for i in range(0, M.shape[0], 2):
+        for j in range(0, M.shape[1], 2):
+            if mode=='sum': 
+                deg2_M[int(i/2), int(j/2)] = M[i,j] + M[i+1,j+1] + M[i+1,j] + M[i,j+1]
+            elif mode=='max': 
+                deg2_M[int(i/2), int(j/2)] = max(M[i,j] + M[i+1,j+1], M[i+1,j] + M[i,j+1])
+            else: 
+                raise ValueError("We currently only accept deg2mode as 'sum' or 'max'. See the code for details. ")
+            
+    return deg2_M
 
 
 def constrainOverlapMatrix(M, max_allowed_distance=999): 
@@ -88,90 +110,41 @@ def constrainOverlapMatrix(M, max_allowed_distance=999):
     return new_M
 
 
-def reorder_bands_OLD(kIdx1, kIdx2, deg=1, max_distance=999, verbosity=0): 
-    """
-    Reorder bands based on a cost matrix.
-    If deg == 1, costMatrix is a square matrix of size nbands * nbands. 
-    Each row represents the bands at kIdx1, 
-    each column represents the bands at kIdx2.
-
-    if deg == 2, costMatrix is a square matrix of size (nbands/2) * (nbands/2). 
-    Each row represents the degenerate subspace at kIdx1, 
-    each column represents the degenerate subspace at kIdx2.
-    
-    Band order at kIdx1 is fixed. 
-    Returns: Array containing the new column orders for kIdx2.
-    """
-    print(f"\nWe are working with kIdx1, kIdx2 = {kIdx1}, {kIdx2}.") if verbosity>0 else None
-    costMatrix = overlapMatrix(kIdx1, kIdx2, deg, verbosity)
-    if deg==1: 
-        nbands = costMatrix.shape[0]
-    elif deg==2: 
-        nbands = costMatrix.shape[0]*2
-
-    constraints = np.zeros((costMatrix.shape[0], costMatrix.shape[0]))
-    for i in range(costMatrix.shape[0]):
-        for j in range(costMatrix.shape[0]):
-            if abs(i - j) > max_distance:
-                constraints[i, j] = 1
-    constrained_cost_matrix = costMatrix - 999999 * constraints
-    row_ind, col_ind = linear_sum_assignment(-constrained_cost_matrix)
-    # row_ind, col_ind = linear_sum_assignment(costMatrix, maximize=True)
-
-    print(f"Max sum value of assignment problem = {costMatrix[row_ind, col_ind].sum()}. This value should be close to nBands = {nbands}.") if verbosity>0 else None
-    if deg==2: 
-        # print(col_ind)
-        col_ind_2deg = np.zeros(nbands, dtype=col_ind.dtype)
-        col_ind_2deg[::2] = col_ind * 2
-        col_ind_2deg[1::2] = col_ind * 2 + 1
-        col_ind = col_ind_2deg
-        # print(col_ind)
-    print(f"New band order at kIdx2: {col_ind}") if verbosity>0 else None
-    return col_ind
-
-
-def reorder_bands_NEW(kIdx, nbands, orderInd_prev, deg=1, max_distance=999, verbosity=0): 
-    print(f"\nWe are re-ordering the eigenvectors at kIdx = {kIdx}.") if verbosity>0 else None
+def reorder_bands(kIdx, nbands, orderInd_prev, deg=1, max_distance=999, deg2mode='sum'): 
+    print(f"\nWe are re-ordering the eigenvectors at kIdx = {kIdx}.")
     newOrder = np.arange(nbands)
     if kIdx==0: 
         return newOrder
     
-    costMatrix = initOverlapMatrix(nbands, deg)
-    costMatrix += overlapMatrix(kIdx-1, kIdx, deg, verbosity)
-
-    constrained_cost_matrix = constrainOverlapMatrix(costMatrix, max_distance)
-    _, col_ind = linear_sum_assignment(constrained_cost_matrix, maximize=True)
-
+    costMatrix = overlapMatrix(kIdx-1, kIdx)[orderInd_prev]   # Remember that it should be reordered by the previous k-point
+    costMatrix = constrainOverlapMatrix(costMatrix, max_distance)
     if deg==2: 
-        # print(col_ind)
+        costMatrix = contractM_deg2(costMatrix, deg2mode)
+
+    _, col_ind = linear_sum_assignment(costMatrix, maximize=True)
+    if deg==2: 
         col_ind_2deg = np.zeros(nbands, dtype=col_ind.dtype)
         col_ind_2deg[::2] = col_ind * 2
         col_ind_2deg[1::2] = col_ind * 2 + 1
         col_ind = col_ind_2deg
-        # print(col_ind)
-    newOrder = orderInd_prev[col_ind]
-    print(f"New band order at kIdx: {newOrder}") if verbosity>0 else None
-    return newOrder
+    print(f"New band order at kIdx: {col_ind}")
+    return col_ind
 
 
-def reorder_bands_NEW_2steps(kIdx, nbands, orderInd_prev2, orderInd_prev1, deg=1, max_distance=999, verbosity=0, prevMultiplier=0.5): 
-    print(f"\nWe are re-ordering the eigenvectors at kIdx = {kIdx}.") if verbosity>0 else None
+# This function is not production ready
+def reorder_bands_2steps(kIdx, nbands, orderInd_prev2, orderInd_prev1, deg=1, max_distance=999, prevMultiplier=0.5, diag=True): 
+    print(f"\nWe are re-ordering the eigenvectors at kIdx = {kIdx}.")
     newOrder = np.arange(nbands)
     if kIdx<=1: 
-        return reorder_bands_NEW(kIdx, nbands, orderInd_prev1, deg, max_distance, verbosity)
+        return reorder_bands(kIdx, nbands, orderInd_prev1, deg, max_distance)
 
-    costMatrix = initOverlapMatrix(nbands, deg)
-    if deg==2: 
-        contracted_orderPrev1 = (np.floor(orderInd_prev1 / 2)[::2]).astype(int)
-        contracted_orderPrev2 = (np.floor(orderInd_prev2 / 2)[::2]).astype(int)
-        costMatrix += overlapMatrix(kIdx-1, kIdx, deg, verbosity)[contracted_orderPrev1]
-        costMatrix += prevMultiplier * overlapMatrix(kIdx-2, kIdx, deg, verbosity)[contracted_orderPrev2]
-    else:
-        costMatrix += overlapMatrix(kIdx-1, kIdx, deg, verbosity)[orderInd_prev1]
-        costMatrix += prevMultiplier * overlapMatrix(kIdx-2, kIdx, deg, verbosity)[orderInd_prev2]
+    if deg==1: 
+        costMatrix = overlapMatrix(kIdx-1, kIdx)[orderInd_prev1] + prevMultiplier * overlapMatrix(kIdx-2, kIdx)[orderInd_prev2]
+    elif deg==2: 
+        costMatrix = contractM_deg2(overlapMatrix(kIdx-1, kIdx)[orderInd_prev1] , diag) + prevMultiplier * contractM_deg2(overlapMatrix(kIdx-2, kIdx)[orderInd_prev2] , diag)
 
-    constrained_cost_matrix = constrainOverlapMatrix(costMatrix, max_distance)
-    _, col_ind = linear_sum_assignment(constrained_cost_matrix, maximize=True)
+    costMatrix = constrainOverlapMatrix(costMatrix, max_distance)
+    _, col_ind = linear_sum_assignment(costMatrix, maximize=True)
 
     if deg==2: 
         # print(col_ind)
@@ -180,24 +153,29 @@ def reorder_bands_NEW_2steps(kIdx, nbands, orderInd_prev2, orderInd_prev1, deg=1
         col_ind_2deg[1::2] = col_ind * 2 + 1
         col_ind = col_ind_2deg
         # print(col_ind)
-    newOrder = col_ind
-    print(f"New band order at kIdx: {newOrder}") if verbosity>0 else None
-    return newOrder
+    print(f"New band order at kIdx: {col_ind}")
+    return col_ind
 
 
-def plotBandStruct_reorder(refGWBS, defaultBS, newOrderBS, bandIdx): 
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-    axs_flat = axs.flatten()
+def plotBandStruct_reorder(refGWBS, defaultBS, newOrderBS, bandIdx, zoomOut=False): 
+    if zoomOut: 
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        axs_flat = axs.flatten()
+    else: 
+        fig, ax = plt.subplots(1, 1, figsize=(7, 7))
+        axs_flat = np.array([None, ax])
 
     # plot ref
     numBands = len(refGWBS[0])
     numKpts = len(refGWBS)
     for i in range(numBands): 
         if i==0: 
-            axs_flat[0].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2, label="Ref GW")
-            axs_flat[1].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2, label="Ref GW")
+            if zoomOut:
+                axs_flat[0].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2, label="Ref GW, default order")
+            axs_flat[1].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2, label="Ref GW, default order")
         else: 
-            axs_flat[0].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2)
+            if zoomOut:
+                axs_flat[0].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2)
             axs_flat[1].plot(np.arange(numKpts), refGWBS[:, i], "b-", alpha=0.1, markersize=2)
             
     # plot default ordering
@@ -205,83 +183,75 @@ def plotBandStruct_reorder(refGWBS, defaultBS, newOrderBS, bandIdx):
     numKpts = len(defaultBS)
     for i in range(numBands): 
         if i==0: 
-            axs_flat[0].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2, label="Default order")
+            if zoomOut:
+                axs_flat[0].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2, label="Default order")
             axs_flat[1].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2, label="Default order")
         else: 
-            axs_flat[0].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2)
+            if zoomOut: 
+                axs_flat[0].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2)
             axs_flat[1].plot(np.arange(numKpts), defaultBS[:, i], "ro-", alpha=0.1, markersize=2)
 
     # plot new ordering
     numKpts = len(newOrderBS)
-    axs_flat[0].plot(np.arange(numKpts), newOrderBS[:, bandIdx], "ro-", alpha=0.8, markersize=2, label=f"New order, band{bandIdx}")
+    if zoomOut:
+        axs_flat[0].plot(np.arange(numKpts), newOrderBS[:, bandIdx], "ro-", alpha=0.8, markersize=2, label=f"New order, band{bandIdx}")
     axs_flat[1].plot(np.arange(numKpts), newOrderBS[:, bandIdx], "ro-", alpha=0.8, markersize=2, label=f"New order, band{bandIdx}")
-            
-    axs_flat[0].legend(frameon=False)
-    axs_flat[0].set(ylim=(-13, 1))
+    
+    if zoomOut:
+        axs_flat[0].legend(frameon=False)
+        axs_flat[0].set(ylim=(-13, 1))
+        axs_flat[0].grid(alpha=0.7)
+    else: 
+        axs_flat[1].legend(frameon=False)
     axs_flat[1].set(ylim=(min(newOrderBS[:, bandIdx])-0.5, max(newOrderBS[:, bandIdx])+0.5))
-    axs_flat[0].grid(alpha=0.7)
     axs_flat[1].grid(alpha=0.7)
 
     fig.tight_layout()
     return fig
 
 
-
 ########################## main ##########################
-testCase = 64    # 16 64 128 150 
 reorderDeg = 2
-lookAhead = 2
+lookAhead = 1
+deg2mode='sum'   # 'sum', 'max'
 
+for testCase in [16, 32, 64, 128, 150]:
+    refGWBS = np.loadtxt(f"CALCS/CsPbI3_test/inputs_{testCase}kpts/expBandStruct_0.par")
+    currentBS = np.loadtxt(f"CALCS/CsPbI3_test/results_{testCase}kpts/initZunger_BS_sys0.dat")
+    print(f"Test Case of {testCase} kpoints. Algorithm assumes strict degeneracy of {reorderDeg}. \n")
 
-'''
-for i in range(80, 100):
-    constrained_cost_matrix = np.loadtxt(f"CALCS/CsPbI3_test/results_{testCase}kpts/overlap_kIdx_{i}_{i+1}_CBQuad.dat")
-    row_ind, col_ind = linear_sum_assignment(-constrained_cost_matrix)
-    # print(row_ind)
-    print(col_ind)
-'''
+    systems, atomPPOrder, nPseudopot, PPparams, totalParams, localPotParams = setAllBulkSystems(1, f"CALCS/CsPbI3_test/inputs_{testCase}kpts/", f"CALCS/CsPbI3_test/results_{testCase}kpts/")
+    sys = systems[0]
+    basis = sys.basis().numpy()
+    print(f"The first 4 entries of the basis vectors: \n {basis[:4]} \n")
+    kpts = sys.kpts.numpy()
+    # print(kpts)
+    kpts_diff = np.diff(kpts, axis=0)
+    print(f"The maximum kpts_diff in any of the three dimensions: {np.max(kpts_diff)}\n")
 
+    if lookAhead==1: 
+        newBS = currentBS[0,1:]
+        prevKidx_order = np.arange(32)
+        for kidx in range(1,testCase):
+            currKidx_order = reorder_bands(kidx, 32, prevKidx_order, reorderDeg, max_distance=8, deg2mode=deg2mode)
+            newE = currentBS[kidx,1:][currKidx_order]
+            newBS = np.vstack((newBS, newE))
+            prevKidx_order = currKidx_order
+    elif lookAhead==2: 
+        raise ValueError("This is an experimental feature. Please stop! ")
+        newBS = currentBS[0,1:]
+        prev2Kidx_order = np.arange(32)
+        prev1Kidx_order = np.arange(32)
 
-refGWBS = np.loadtxt(f"CALCS/CsPbI3_test/inputs_{testCase}kpts/expBandStruct_0.par")
-currentBS = np.loadtxt(f"CALCS/CsPbI3_test/results_{testCase}kpts/epoch_1_BS_sys0.dat")
-print(f"Test Case of {testCase} kpoints. Algorithm assumes strict degeneracy of {reorderDeg}. \n")
+        for kidx in range(1,testCase):
+            currKidx_order = reorder_bands_2steps(kidx, 32, prev2Kidx_order, prev1Kidx_order, reorderDeg, max_distance=4, prevMultiplier=0.7, diag=diag)
+            newE = currentBS[kidx,1:][currKidx_order]
+            newBS = np.vstack((newBS, newE))
+            prev2Kidx_order = prev1Kidx_order
+            prev1Kidx_order = currKidx_order
 
-systems, atomPPOrder, nPseudopot, PPparams, totalParams, localPotParams = setAllBulkSystems(1, f"CALCS/CsPbI3_test/inputs_{testCase}kpts/", f"CALCS/CsPbI3_test/results_{testCase}kpts/")
-sys = systems[0]
-basis = sys.basis().numpy()
-print(f"The first 4 entries of the basis vectors: \n {basis[:4]} \n")
-kpts = sys.kpts.numpy()
-# print(kpts)
-kpts_diff = np.diff(kpts, axis=0)
-print(f"The maximum kpts_diff in any of the three dimensions: {np.max(kpts_diff)}\n")
-
-if lookAhead==1: 
-    newBS = currentBS[0,1:]
-    prevKidx_order = np.arange(32)
-    for kidx in range(1,testCase):
-        currKidx_order = reorder_bands_NEW(kidx, 32, prevKidx_order, reorderDeg, max_distance=4, verbosity=0)
-        newE = currentBS[kidx,1:][currKidx_order]
-        newBS = np.vstack((newBS, newE))
-        prevKidx_order = currKidx_order
-elif lookAhead==2:
-    newBS = currentBS[0,1:]
-    prev2Kidx_order = np.arange(32)
-    prev1Kidx_order = np.arange(32)
-
-    currKidx_order = reorder_bands_NEW(1, 32, prev2Kidx_order, reorderDeg, max_distance=4, verbosity=0)
-    newE = currentBS[1,1:][currKidx_order]
-    newBS = np.vstack((newBS, newE))
-    prev1Kidx_order = currKidx_order
-
-    for kidx in range(2,testCase):
-        currKidx_order = reorder_bands_NEW_2steps(kidx, 32, prev2Kidx_order, prev1Kidx_order, reorderDeg, max_distance=4, verbosity=0, prevMultiplier=1.0)
-        newE = currentBS[kidx,1:][currKidx_order]
-        newBS = np.vstack((newBS, newE))
-        prev2Kidx_order = prev1Kidx_order
-        prev1Kidx_order = currKidx_order
-
-for bandIdx in range(32):
-    fig = plotBandStruct_reorder(refGWBS[:,1:], currentBS[:,1:], newBS, bandIdx)
-    fig.savefig(f"CALCS/CsPbI3_test/results_{testCase}kpts/newBand_deg{reorderDeg}_{bandIdx}.png")
-    plt.close()
+    for bandIdx in range(32):
+        fig = plotBandStruct_reorder(refGWBS[:,1:], currentBS[:,1:], newBS, bandIdx)
+        fig.savefig(f"CALCS/CsPbI3_test/results_{testCase}kpts/reorder_results/newBand_deg{reorderDeg}_{bandIdx}_{deg2mode}.pdf")
+        plt.close()
 
